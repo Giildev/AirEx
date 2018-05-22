fbb681b728f47b454ce6baad4866ea3c9bf93051
nilson_tns <nilsontns@gmail.com>
2018-05-21 21:45:16 -0400

    user/porfile and user/edit backend

--- models/user.js
+++ models/user.js
@@ -5,6 +5,8 @@ const Schema = mongoose.Schema;
 var UserSchema = new Schema(
   {
     name: { type: String, required: true },
+    lastName: { type: String },
+    phone: { type: String },
     password: { type: String, required: true },
     email: { type: String, unique: true, lowercase: true, required: true },
     token: { type: String }
@@ -24,7 +26,7 @@ UserSchema.pre('save', function(next) {
   bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
       if (err) return next(err);
 
-      // hash the password using our new salt
+      // hash the password along with our new salt
       bcrypt.hash(user.password, salt, function(err, hash) {
           if (err) return next(err);
 
